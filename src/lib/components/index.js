a05c48e5bb98300e7ec46c0d2b51b64354d960ce
Bruce Stockwell <bruce.stockwell@gmail.com>
2021-11-01 14:54:33 -0400

    clean up and new index file for exports

---
+++ src/lib/components/index.js
@@ -0,0 +1,5 @@
+export { default as BannerMessage } from "./BannerMessage";
+export * from "./BannerMessage";
+
+export { default as RowContainer } from "./RowContainer";
+export * from "./RowContainer";
