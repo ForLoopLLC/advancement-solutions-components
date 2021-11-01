41f6374b15e353cb8e1137e6ec333e4d35cf5281
Bruce Stockwell <bruce.stockwell@gmail.com>
2021-11-01 15:37:33 -0400

    compressed the Banner message margins and added media query

--- src/lib/components/BannerMessage/BannerMessage.js
+++ src/lib/components/BannerMessage/BannerMessage.js
@@ -1,28 +1,34 @@
 import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 import { Typography } from "@mui/material";
 import WarningIcon from "@mui/icons-material/Warning";
 import RowContainer from "../RowContainer";
 
 const BannerMessage = ({ text }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <RowContainer
       sx={{
         backgroundColor: "info.main",
       }}
     >
-      <WarningIcon
-        sx={{
-          mt: 2,
-          mb: 2,
-          color: "info.contrastText",
-        }}
-      />
+      {!isMobile && (
+        <WarningIcon
+          sx={{
+            mt: 1,
+            mb: 1,
+            mr: 1,
+            color: "info.contrastText",
+          }}
+        />
+      )}
       <Typography
         variant="body1"
         sx={{
-          mt: 2,
-          mb: 2,
-          ml: 2,
+          mt: 1,
+          mb: 1,
           color: "info.contrastText",
         }}
       >
