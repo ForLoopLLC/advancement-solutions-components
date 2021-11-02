5ed21fba16e9d4b539fe79458b37fff0a1af395b
Bruce Stockwell <bruce.stockwell@gmail.com>
2021-11-02 16:02:08 -0400

    troubleshooting page export

--- src/lib/components/Page/Page.js
+++ src/lib/components/Page/Page.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
-import { BannerMessage } from "../BannerMessage";
+//import { BannerMessage } from "../BannerMessage";
 import { Box } from "@mui/system";
 
 const Page = ({ user, children, busy = false }) => {
   return (
     <Box>
-      {user && user.message && <BannerMessage text={user.message} />}
       <Box
         sx={{
           padding: 2,
